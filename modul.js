bd2c3a1ee6ad874b464405347101bd040b9750c7
Avijit-126 <156359974+Avijit-126@users.noreply.github.com>
2024-05-19 13:09:26 +0530

    Update modul.js

--- modul.js
+++ modul.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://0.0.0.0:27017/Note")
+  .connect(process.env.mongodb_url)
   .then(() => {
     console.log("connection Successful");
   })
